a2e73f60ed9e305c5f837b5c2975fc664acb9d9f
Ubuntu <ubuntu@ip-172-31-39-184.eu-west-2.compute.internal>
2025-06-09 02:12:27 +0000

    Update get_extensions.sh with select menus and improved input

--- get_extensions.sh
+++ get_extensions.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-read -p "what is your name?: " name
-read -p "what is your surname/family name?: " surname
-read -s -N 4 -p "what is your extension number? (max 4 digits): " extension
+# Ask for the user's first name and store it in the variable 'name'
+read -p "What is your name?: " name
 
-echo    # <<< adds a newline
+# Ask for the user's surname and store it in the variable 'surname'
+read -p "What is your surname/family name?: " surname
 
-read -s -N 4 -p "what access code would you like to use when dialing in? (max 4 digits): " code
+# Present phone type options using the 'select' menu
+PS3="What phone would you like to work with the new system?: "
+select phone in Headset Handheld; do
+  # Store the selected phone option in the variable 'phone' and show confirmation
+  echo "You will receive a $phone phone"
+  break
+done
 
-echo    # <<< adds a newline
+# Present department options using the 'select' menu
+PS3="What is your department?: "
+select department in Finance Sales Customer_Service Engineering; do
+  # Store the selected department in the variable 'department' and show confirmation
+  echo "Your department is $department"
+  break
+done
 
+# Ask the user for their 4-digit extension number (input hidden as they type)
+read -s -N 4 -p "What is your extension number? (max 4 digits): " extension
+echo    # (adds a newline for better formatting after silent input)
 
-# Set CSV file name
+# Ask the user for their 4-digit access code (input hidden as they type)
+read -s -N 4 -p "What access code would you like to use when dialing in? (max 4 digits): " code
+echo    # (adds a newline for better formatting after silent input)
+
+# Set the name of the CSV file where the information will be saved
 csv_file="extension.csv"
 
-# Append the data to the CSV
+# Generate a timestamp for when the entry was made
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$timestamp,$name,$surname,$extension,$code" >> "$csv_file"
 
+# Append all collected data to the CSV file in a comma-separated format
+echo "$timestamp,$name,$surname,$department,$phone,$extension,$code" >> "$csv_file"
+
+# Confirm to the user that the data has been saved
 echo "Your information has been saved to $csv_file."
+
+
